ff0d95d04c0be1af08eb33a90c4dce380f370f5d
shajibshahriare <shajib.shahriare16@gmail.com>
2024-03-12 23:59:59 +0600

    basic header and blogs section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
 import Blogs from './components/Blogs/Blogs'
+import Bookmarks from './components/Bookmarks/Bookmarks'
 import Header from './components/Header/Header'
 
 function App() {
@@ -8,7 +9,10 @@ function App() {
   return (
     <>
       <Header></Header>
+      <div className='lg:flex container mx-auto mt-5'>
       <Blogs></Blogs>
+      <Bookmarks></Bookmarks>
+      </div>
     </>
   )
 }
---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,10 @@
+
+const Bookmarks = () => {
+    return (
+        <div className="lg:w-1/3">
+          <h1>Bookmarks</h1>  
+        </div>
+    );
+};
+
+export default Bookmarks;
